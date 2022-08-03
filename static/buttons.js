06777befac2f80ba9e96d13afbad46b1f2959295
Pieczarkaa <108304044+Pieczarkaa@users.noreply.github.com>
2022-08-03 16:08:50 +0200

    Add files via upload

---
+++ static/buttons.js
@@ -0,0 +1,7 @@
+function invite() {
+	window.open("https://discord.com/api/oauth2/authorize?client_id=1004385276459757638&permissions=277028603968&scope=bot%20applications.commands","__blank")
+}
+
+function supportServer() {
+	window.open("https://discord.gg/TsqkpxxaTf","__blank")
+}
